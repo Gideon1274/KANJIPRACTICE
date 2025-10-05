ce53041e8d8e9524e705224e522b67551f3a8349
Gideon1274 <gideongrid127@gmail.com>
2025-10-06 02:56:19 +0800

    deleted:    Kanafolder/kana.html 	deleted:    Kanafolder/kanaData.js 	deleted:    N5l ast.txt 	modified:   vocabfolder/Data/asd.js 	deleted:    vocabfolder/Data/hardvocab.js 	modified:   vocabfolder/Data/n3.js 	modified:   vocabfolder/Data/n4.js 	modified:   vocabfolder/Data/n5.js 	deleted:    vocabfolder/Data/spanish.js 	modified:   vocabfolder/VOCAB.html 	new file:   vocabfolder/ambot.html

--- vocabfolder/Data/asd.js
+++ vocabfolder/Data/asd.js
@@ -1,11 +1,20 @@
     
         const kanjiData = {
             N5: [
-
-
-
-
-
+        { kanji: "影響", readings: ["えいきょう", "eikyō"], meanings: ["influence", "effect"] },
+        { kanji: "営業", readings: ["えいぎょう", "eigyō"], meanings: ["business", "sales"] },
+        { kanji: "衛星", readings: ["えいせい", "eisei"], meanings: ["satellite", "moon"] },
+        // { kanji: "栄養", readings: ["えいよう", "eiyō"], meanings: ["nutrition", "nourishment"] },
+        // { kanji: "延期", readings: ["えんき", "enki"], meanings: ["postponement", "deferment"] },
+        // { kanji: "演技", readings: ["えんぎ", "engi"], meanings: ["acting", "performance"] },
+        // { kanji: "演劇", readings: ["えんげき", "engeki"], meanings: ["drama", "theatre"] },
+        // { kanji: "援助", readings: ["えんじょ", "enjo"], meanings: ["assistance", "aid"] },
+        // { kanji: "演説", readings: ["えんぜつ", "enzetsu"], meanings: ["speech", "address"] },
+        // { kanji: "演奏", readings: ["えんそう", "ensō"], meanings: ["musical performance"] },
+       // { kanji: "応援", readings: ["おうえん", "ōen"], meanings: ["aid", "cheering"] },
+        // { kanji: "往復", readings: ["おうふく", "ōfuku"], meanings: ["round trip", "correspondence"] },
+        // { kanji: "欧米", readings: ["おうべい", "ōbei"], meanings: ["Europe and America", "the West"] },
+        // { kanji: "応用", readings: ["おうよう", "ōyō"], meanings: ["application", "applied"] },
 
 
 
