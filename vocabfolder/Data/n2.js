dfe69f0190c19e1009396efae7335e7f38e0dd4a
gideongrid127@gmail.com <gideongrid127@gmail.com>
2026-01-02 21:38:13 +0800

    new file:   vocabfolder/Data/n1.js 	new file:   vocabfolder/Data/n2.js 	modified:   vocabfolder/Data/n3.js 	modified:   vocabfolder/Data/n4.js 	modified:   vocabfolder/Data/n5.js 	modified:   vocabfolder/VOCAB.html 	deleted:    vocabfolder/ambot.html 	deleted:    vocabfolder/basic.html 	deleted:    vocabfolder/kanji.html
    	new file:   vocabfolder/Data/n1.js
    	new file:   vocabfolder/Data/n2.js
    	modified:   vocabfolder/Data/n3.js
    	modified:   vocabfolder/Data/n4.js
    	modified:   vocabfolder/Data/n5.js
    	new file:   vocabfolder/Data/temp.js
    	renamed:    vocabfolder/basic.html -> vocabfolder/TEMP.HTML
    	modified:   vocabfolder/VOCAB.html
    	deleted:    vocabfolder/ambot.html
    	deleted:    vocabfolder/kanji.html

---
+++ vocabfolder/Data/n2.js
@@ -0,0 +1,9 @@
+// JLPT N2 vocabulary/kanji dataset.
+// Format must match the quiz engine: { kanji: string, readings: string[], meanings: string[] }
+// Populate this list from a source you have rights to use.
+
+window.kanjiData = window.kanjiData || {};
+window.kanjiData.N2 = [
+    // Example:
+    // { kanji: "例", readings: ["れい", "rei"], meanings: ["example"] },
+];
